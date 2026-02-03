df03aef9b8d2f89c2108e3517e5c3cc94e4f5085
Sithu <cithukyaw@gmail.com>
2026-02-03 11:55:17 +0630

    Remove util function getGeminiVisionPayload

--- utils.js
+++ utils.js
@@ -7,20 +7,6 @@ export const trimOutput = (outputText) => outputText
   .replace(/\s*```\s*$/i, '')
   .trim();
 
-export const getGeminiVisionPayload = base64ImageFile => {
-  return [
-    {
-      text: "Extract the Burmese texts from the image as json format where the key will be a word for category and it has a list of definitions. The category word should prepend to each definition without using any character except a space. Do not extract page number, header and footer."
-    },
-    {
-      inlineData: {
-        mime_type: "image/jpeg",
-        data: base64ImageFile
-      }
-    }
-  ]
-}
-
 export const getPrompt = () => {
   return "Extract the Burmese texts from the image as JSON format where the key means category and it has a group of definitions. The category word should prepend to each definition without using any character except a space. No need the static key 'category' and 'definitions'. Do not extract page number, header and footer."
 }
